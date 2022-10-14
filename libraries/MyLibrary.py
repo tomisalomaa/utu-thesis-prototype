114f9cdb1baa20854e280c38f81b9783665bf4ac
tomisalomaa <salomaa.tomi@gmail.com>
2022-10-15 00:04:47 +0300

    MyLibrary commented

--- libraries/MyLibrary.py
+++ libraries/MyLibrary.py
@@ -1,3 +1,16 @@
+# Library to support various parsing tasks.
+# Each method is usable as a keyword when imported in a Robot file.
+# Robot framework logic:
+# - Method name translates into a keyword when underscores are left out.
+#   For example: def prepare_soup() in this library ==> 'Prepare Soup' in robot file
+# - When used as robot keywords after library is imported,
+#   keywords require input values unless a default value is declared for an input variable in this library
+# - Return in method means the keyword will return content when used
+#
+# Do note that whenever html parser is used on source content, a lot of syntactic errors present in the
+# raw source code will be corrected by the html parses in the process. If the purpose is to receive
+# raw content, regular expressions are recommended.
+
 # coding=utf-8
 import os
 import re
@@ -7,13 +20,21 @@ import errno
 from bs4 import BeautifulSoup, Doctype
 
 class MyLibrary:
-    # General support helper method
+    # While this will be available as a keyword when imported,
+    # this is not meant to be used as such.
+    # Used as a support method within this class to parse content
+    # with Beautiful Soup.
     def prepare_soup(self, src_file, parser):
+        '''
+        :param src_file: file path
+        :param parser: parser to use for parsing content from src_file; current prototype includes support for html5lib and lxml
+        '''
         with open(src_file) as src:
             soup = BeautifulSoup(src, parser)
         return soup
 
-    # General support
+    # Searches a given path contents for files matching a given file extension.
+    # The path given as input is a starting point, will also search from all sub-directories.
     def search_file_with_extension(self, path, file_extension):
         '''
         Returns all file locations found with specified extension
@@ -33,64 +54,83 @@ class MyLibrary:
                     file_locations.append(file_location)
         return file_locations
 
-    # Parse support - cannot be used in static assessment since html parser "corrects" mistakes
-    def search_doctype_from_html(self, src, parser='html5lib'):
-        items = []
-        soup = self.prepare_soup(src, parser)
-        for item in soup.contents:
-            if isinstance(item, Doctype):
-                items.append(item)
-        return items
-
-    def format_soup_tag_to_string(self, soup):
-        string_form = []
-        for item in soup:
-            string_form.append(str(item))
-        return string_form
-
-    # Parse support
+    # Parses the source contents for all html elements containing the attribute 'id'.
+    # Found elements form a bs4 result set (a list) of bs4 tags.
     def find_all_ids_from_html(self, src, parser='html5lib'):
+        '''
+        :param src: file path
+        :param parser: html5lib by default; if provided as input,
+                        make sure another parser library is supported.
+        '''
         soup = self.prepare_soup(src, parser)
         found_ids = soup.find_all(id=True)
         return found_ids
 
-    # Parse support
+    # Searches for specific element from the source contents.
+    # Search ends as soon as the very first matching instance is found.
     def find_element_from_html(self, src, elem, parser='html5lib'):
+        '''
+        :param src: file path
+        :param parser: html5lib by default; if provided as input,
+                        make sure another parser library is supported.
+        '''
         soup = self.prepare_soup(src, parser)
         found_element = soup.find(elem)
         return found_element
     
-    # Parse support
+    # See find_element_from_html(); this one does the same but
+    # returns all found elements as a list of bs4 tags.
     def find_elements_from_html(self, src, elem, parser='html5lib'):
+        '''
+        :param src: file path
+        :param parser: html5lib by default; if provided as input,
+                        make sure another parser library is supported.
+        '''
         soup = self.prepare_soup(src, parser)
         found_elements = soup.find_all(elem)
         return found_elements
     
-    # Parse support
+    # Finds elements that have a specific attribute. Attribute values are of no concern.
+    # Returns all matching elements as tags.
     def find_elements_with_attribute(self, src, elem_tag, attr, parser='html5lib'):
+        '''
+        :param src: file path
+        :param elem_tag: html element tag to search for, i.e. a, table, li, ul...
+        :param attr: attribute the elements should contain, i.e. name, id, class...
+        :param parser: html5lib by default; if provided as input,
+                        make sure another parser library is supported.
+        '''
         soup = self.prepare_soup(src, parser)
         found_elements = soup.find_all(elem_tag, {attr:True})
         return found_elements
     
-    # Parse support
+    # Searches for and lists all child elements of a given bs4 tag.
     def find_immediate_child_elements(self, src):
+        '''
+        :param src: a bs4 html element tag
+        '''
         children = [child for child in src if child.name != None]
         return children
 
-    # Parse support
+    # Searches for elements that have a given class.
     def find_elements_by_class(self, src, elem, cls, parser='html5lib'):
+        '''
+        :param src: file path
+        :param elem: html element tag to look for
+        :param cls: class that element should contain
+        '''
         soup = self.prepare_soup(src, parser)
         elems = soup.select(f'{elem}.{cls}')
         return elems
 
-    # Parse support
-    '''
-    Does not fix the raw content as html parsers do;
-    performs string searches from raw text source.
-    Returns a multi-line string containing child elements
-    from all the found matches.
-    '''
+    # Finds elements from source contents. Does not fix the raw content as html parsers do
+    # due to not using Beautiful Soup and html parsers.
+    # Returns a multi-line string containing child elements from all the found matches.
     def find_elements_from_raw_source(self, src, elem):
+        '''
+        :param src: file path
+        :param elem: element to look for; case is ignored
+        '''
         element_results = []
         with open(src) as src_open:
             source_raw = src_open.read()
@@ -105,19 +145,20 @@ class MyLibrary:
             element_results.append(elem)
         return element_results
 
-    # Parse support
-    '''
-    Does not fix the raw content as html parsers do;
-    performs string searches from raw text source.
-    '''
+    # Expects a list of elements contained within html ul/ol/menu element.
+    # Forms a dictionary based on input list contents so that
+    # each key in the dictionary has child elements as values.
     def parent_child_relations_from_list(self, str_list):
+        '''
+        :param str_list:    a list of elements contained within a single html ul / ol / menu element;
+                            elements must be in string format.
+                            A proper input can be got from find_elements_from_raw_source(),
+                            for example.
+        '''
         length = len(str_list)
         parent_child_dict = {}
         met_parents = []
-        print(met_parents)
         for i in range(length):
-            print(met_parents)
-            print(str_list[i][1])
             if str_list[i][1] != '/':
                 if i-1 >= 0:
                     if met_parents[0] in parent_child_dict:
@@ -128,6 +169,7 @@ class MyLibrary:
             else:
                 met_parents.pop(0)
         return parent_child_dict
+
 
     def copy_directory_contents(self, src, dst):
         try:
